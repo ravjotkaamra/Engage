19bdf4ec10603bf033f985f85b6718364a73306f
ravjot <ravjotkaamra@gmail.com>
2021-07-04 21:51:10 +0530

    uibuild

--- client/src/services/agora.js
+++ client/src/services/agora.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3001/api/rtctoken';
+const baseUrl = '/api/rtctoken';
 
 const fetchAgoraToken = async (channel, uid, isPublisher = true) => {
   const body = {
